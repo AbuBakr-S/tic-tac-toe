04d9f887e0461af431c522c8cb7ec6c9d9553e26
Abubakar Siddique <abz.siddique@gmail.com>
2019-06-17 11:28:19 +0100

    Importing JS objects to interact with the DOM and to access react methods

--- my-tictactoe-app/src/index.js
+++ my-tictactoe-app/src/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
 // There are currently no interactive components.
 
 // The Square component renders a single <button>.
